d271a2597cc4ee11e0ab6b90ba425222dfe312b1
frank-lam <1002039945@qq.com>
2017-03-10 16:15:30 +0800

    test

--- 20170310.cpp
+++ 20170310.cpp
@@ -1,5 +1,4 @@
-
-// ##1.全排列问题 STL的应用
+1.全排列问题 STL的应用
 // 参考 http://www.slyar.com/blog/stl_next_permutation.html
 // C++库常用函数一览 - candyliuxj - 博客频道 - CSDN.NET
 // http://blog.csdn.net/candyliuxj/article/details/6313191
@@ -22,3 +21,4 @@ int main(){
 		puts("");
 	}
 }
+

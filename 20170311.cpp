1d4b4a0d0c60ab1faf33a0004f50963d6d8bd963
frank-lam <1002039945@qq.com>
2017-03-11 19:08:44 +0800

    12

--- 20170311.cpp
+++ 20170311.cpp
@@ -1,3 +1,9 @@
+/**************************************************************
+    date: 20170311
+    User: frank_ecnu
+    Language: C++
+    Accepted: 4
+****************************************************************/
 1074：对称平方数
 
 #include <iostream>
@@ -55,5 +61,79 @@ int main(){
 			}
 			cout << endl;
 		}
+	}
+}
+
+
+1112：拦截导弹
+#include <iostream>
+using namespace std;
+
+int max(int a,int b){
+	return a>b?a:b;
+}
+int main(){
+	int dp[100],list[100];
+	int n;
+	int maxdp;
+
+	while(cin >> n){
+		for(int i=1;i<=n;i++)
+			cin >> list[i];
+
+		for(int i=1;i<=n;i++){
+			maxdp=1;
+			for(int j=1;j<i;j++){
+				if(list[i]<=list[j]){
+					maxdp=max(maxdp,dp[j]+1);
+				}
+			}
+			dp[i]=maxdp;
+		}
+		int maxnumber=dp[1];
+		for(int i=2;i<=n;i++)
+			maxnumber=max(dp[i],maxnumber);
+		cout << maxnumber << endl;
+	}
+}
+
+
+1111：单词替换
+九度题目1111：单词替换 - 点十八的一方天地 - 博客频道 - CSDN.NET
+http://blog.csdn.net/fnzsjt/article/details/40432257
+
+注意这道题目的陷阱：
+替换的是单词，如果
+
+Youwant someone to help you
+
+You
+
+I
+
+这样是替换不了的。下面给出代码：
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <string.h>
+using namespace std;
+int main(){
+	string s,a,b;
+	while(getline(cin,s)){
+		getline(cin,a);
+		getline(cin,b);
+		s=" "+s+" ";
+		a=" "+a+" ";
+		b=" "+b+" ";
+		int first=s.find(a);
+		int len=a.size();
+		if(first!=string::npos){
+			s.replace(first,len,b);
+		}
+		s=s.replace(s.begin(),s.begin()+1,"");
+		s=s.replace(s.size()-1,1,"");
+		cout << s << endl;
 	}
 }
